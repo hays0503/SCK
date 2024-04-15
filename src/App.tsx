697ed5502b0a0de65a32bea29702ce56322ccfc0
Sergey Pimenov <cthtufgbv@mail.ru>
2024-04-15 13:35:33 +0500

    Отредактировал стили под мобильные устройства

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,52 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import sck from '/sck-logo.svg'
-import style from './App.module.scss'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import sck from "/sck-logo.svg";
+import style from "./App.module.scss";
 
 function App() {
-
   return (
     <div className={style.container}>
-      <div className={style.spinerDiv}>
-        <a href="https://sck-1.kz" target="_blank">
-          <img src={sck} className={style.logo} alt="sck-1 logo" />
-        </a>
-        Vite+React
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={reactLogo}  className={style.logo} alt="React logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={viteLogo} className={`${style.logo} ${style.react}`}  alt="Vite logo"/>
-        </a>
+      <div className={style.spinContainer}>
+        
+        <div className={`${style.spinerDiv}`}>
+          <a className={style.z1} href="https://sck-1.kz" target="_blank">
+            <img src={sck} className={style.logo} alt="sck-1 logo" />
+          </a>
+        </div>
+
+        <div className={`${style.spinerDiv}`}>
+          <a className={style.z1} href="https://vitejs.dev" target="_blank">
+            <img src={reactLogo} className={style.logo} alt="React logo" />
+          </a>
+        </div>
+
+        <div className={`${style.spinerDiv}`}>
+          <a className={style.z1} href="https://react.dev" target="_blank">
+            <img
+              src={viteLogo}
+              className={`${style.logo} ${style.react}`}
+              alt="Vite logo"
+            />
+          </a>
+        </div>
+
+        <div className={`${style.spinerDiv}`}><h5>Vite+React</h5></div>
       </div>
+
       <h1>Тестирование интеграций</h1>
       <div className={style.readTheDocs}>
         <h3>
-          <a href='https://m.binotel.kz/'>Binotel</a> + <a href='https://www.jivosite.com/'>Jivosite</a> 
-          (скрипты вырезан с сайта <a href='https://sck-1.kz'>sck-1.kz</a>)
+          <a href="https://m.binotel.kz/">Binotel</a> +{" "}
+          <a href="https://www.jivosite.com/">Jivosite</a>
+          (скрипты вырезан с сайта <a href="https://sck-1.kz">sck-1.kz</a>)
         </h3>
-        <h3>Тестирование интеграции функций с сайта  <a href='https://sck-1.kz'>sck-1.kz</a></h3>
-      </div>
+        <h3>
+          Тестирование интеграции функций с сайта{" "}
+          <a href="https://sck-1.kz">sck-1.kz</a>
+        </h3>
+        </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
